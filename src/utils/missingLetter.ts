e7aa3a19386cfb9002491f0a932a38764cf2724b
Eliabe dos Santos <72847444+eliabedossantos@users.noreply.github.com>
2025-06-26 12:50:59 -0300

    fix: fix build issues

--- src/utils/missingLetter.ts
+++ src/utils/missingLetter.ts
@@ -2,8 +2,8 @@ export function getMissingLetter(name: string): string {
     const normalized = name
         .toLowerCase()
         .normalize('NFD') 
-        .replace(/[\u0300-\u036f]/g, '') //remove accents
-        .replace(/[^a-z]/g, ''); //remove numbers, special chars etc
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/[^a-z]/g, '');
 
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
     for (const letter of alphabet) {
